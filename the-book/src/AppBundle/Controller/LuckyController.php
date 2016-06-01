37b891e91487c1fd1815755b1c66ef93e3e433e6
Lê Thanh Bình <binh@sunrise.vn>
2016-06-01 23:18:32 +0700

    Chapter 4: creating a page

--- the-book/src/AppBundle/Controller/LuckyController.php
+++ the-book/src/AppBundle/Controller/LuckyController.php
@@ -2,11 +2,43 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends Controller
 {
+    /**
+     * @Route("/lucky/number/twig/{count}", name="luck_number_twig")
+     */
+    public function numberCountTwigAction($count = 10)
+    {
+        $numbers = array('<ol>');
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = '<li>' . rand(0, 100) . '</li>';
+        }
+        $numbers[] = '</ol>';
+        $numbersList = implode(' ', $numbers);
+
+        return $this->render('lucky/number.html.twig', array('luckyNumberList' => $numbersList));
+    }
+
+    /**
+     * @Route("/lucky/number/{count}")
+     */
+    public function numberCountAction($count)
+    {
+        $numbers = array();
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = '<li>' . rand(0, 100) . '</li>';
+        }
+        $numbersList = implode(' ', $numbers);
+
+        return new Response(
+            '<html><body>Lucky numbers: <ol>' . $numbersList . '</ol></body></html>'
+        );
+    }
+
     /**
      * @Route("/lucky/number")
      */
--- the-book/tests/AppBundle/Controller/LuckyControllerTest.php
+++ the-book/tests/AppBundle/Controller/LuckyControllerTest.php
@@ -5,17 +5,37 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LuckyControllerTest extends WebTestCase
 {
-    public function testShowNumber()
+    public function luckyNumberCountProvider()
+    {
+        return array(array('1'), array('2'), array('3'), array('4'), array('5'), array('6'), array('7'));
+    }
+
+    /**
+     * @dataProvider luckyNumberCountProvider
+     */
+    public function testLuckyNumberCount($count)
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/lucky/number/' . $count);
+        $this->assertEquals($count, $crawler->filter('li')->count());
+        $crawlerTwig = $client->request('GET', '/lucky/number/twig/' . $count);
+        $this->assertEquals($count, $crawlerTwig->filter('li')->count());
+
+    }
+
+    public function testShowLuckyNumber()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/lucky/number');
         $this->assertGreaterThan(0, $crawler->filter('html:contains("My lucky number is")')->count());
     }
-    public function testLinkClicking(){
+
+    public function testLuckyNumberLinkClicking()
+    {
         $client = static::createClient();
         $crawler = $client->request('GET', '/lucky/number');
         $link = $crawler->filter('a:contains("Greet")')->eq(0)->link();
         $crawler = $client->click($link);
-        $this->assertEquals(200,$client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
